34e13156ac2cd169d14fc658edd6fc1ba388b44f
Hopkins2004 <138828818+Hopkins2004@users.noreply.github.com>
2025-01-25 14:01:57 +0100

    Add files via upload
    Basic model train sur training_i.csv (éléments de 2000 à 9400) et teste sur test_i.csv(éléments 10 à 100)
    Pour créer des nouvelles itérations, il faut run main.py 2 fois (un pour test, un pour training) en rajoutant des nouvelles features

---
+++ main.py
@@ -0,0 +1,29 @@
+import csv
+
+# Open the original file and process the data
+input_csv = 'train.csv'
+output_csv = 'test13.csv'
+
+with open(input_csv, 'r') as infile, open(output_csv, 'w', newline='') as outfile:
+    reader = csv.reader(infile)
+    writer = csv.writer(outfile)
+
+    header = next(reader)  # Read the header from the original file
+    molecules = []
+    for row in reader:
+        molecules.append(row)
+
+    for row in molecules[10:100]:
+        co = row[1:2248]
+        ecfc = row[200:2248]
+        tot = 0
+        for element in ecfc:
+            if element!=0:
+                tot+=1
+        co.append(tot)
+        label = row[-1]
+        writer.writerow(co + [label])
+
+print(f"Processed data has been saved to {output_csv}")
+
+
